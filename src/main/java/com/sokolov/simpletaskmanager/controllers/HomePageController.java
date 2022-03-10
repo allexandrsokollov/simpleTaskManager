4b08fe256e126acabef3db26920d29b8e7ec1387
Alexandr <allexandersokollov@gmail.com>
2022-03-10 12:25:46 +0300

    add test home page with the picture

---
+++ src/main/java/com/sokolov/simpletaskmanager/controllers/HomePageController.java
@@ -0,0 +1,13 @@
+package com.sokolov.simpletaskmanager.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomePageController {
+
+    @GetMapping("/")
+    public String home() {
+        return "home";
+    }
+}
---
+++ src/test/java/com/sokolov/simpletaskmanager/HomePageTest.java
@@ -0,0 +1,30 @@
+package com.sokolov.simpletaskmanager;
+
+import com.sokolov.simpletaskmanager.controllers.HomePageController;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(HomePageController.class)
+public class HomePageTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void testHomePageController() throws Exception {
+        mockMvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("home"))
+                .andExpect(content().string(
+                        containsString("This is home page")));
+    }
+}
